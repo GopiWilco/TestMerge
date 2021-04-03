ec04e71e210e008a3455c3024f77647b5104bf49
Gopi <gopis@wilcosource.com>
2021-04-03 11:45:18 +0530

    COmmit 1

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Amazon;
+using Amazon.QuickSight;
+using Amazon.QuickSight.Model;
+using Amazon.Runtime;
+using WebApplication1.Models.QuickSight;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //QuicksightDashboard.CreateQuickSightGroup();
+
+            var url = QuicksightDashboard.GetUrl();
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Amazon.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplication1.Models
+{
+    using Amazon;
+    using Amazon.QuickSight;
+    using Amazon.QuickSight.Model;
+    using Amazon.Runtime;
+    using Amazon.SecurityToken;
+    using Amazon.SecurityToken.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Amazon.CognitoIdentity;
+    using System.Web;
+    using System.Collections.Specialized;
+
+    namespace QuickSight
+    {
+        public class QuicksightDashboard
+        {
+          public void MasterMethod()
+{
+}
+        }
+    }
+}
